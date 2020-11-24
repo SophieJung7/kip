d3845a741cb69b067f47bf642a986f42f5d74516
Chris Ashe <chris.ashe@gmail.com>
2020-11-24 20:47:18 +0800

    Fix broken unit test

--- subset.test.js
+++ subset.test.js
@@ -1,6 +1,6 @@
 const isSubset = require('./subset');
 
-test('2nd array is subset of 1st array', () => {
-  const result = isSubset();
-  expect(result).toBe('hello');
+test('2nd array is subset of 1st array returns true', () => {
+  const result = isSubset(['a', 'b', 'c'], ['a']);
+  expect(result).toBe(true);
 });
