b44c5918c07e3a8f4f386bc0cbde970d40a48cb3
Chris Ashe <chris.ashe@gmail.com>
2020-11-24 20:48:11 +0800

    Add unit test covering negative case.

--- subset.test.js
+++ subset.test.js
@@ -4,3 +4,8 @@ test('2nd array is subset of 1st array returns true', () => {
   const result = isSubset(['a', 'b', 'c'], ['a']);
   expect(result).toBe(true);
 });
+
+test('2nd array is not subset of 1st array returns false', () => {
+  const result = isSubset(['a', 'b', 'c'], ['d']);
+  expect(result).toBe(false);
+});
